36440d370c820e94c7002953b169be820d03c425
Stutay Monga <Stutay.Monga9@gmail.com>
2017-09-27 21:45:11 -0400

    Create Chat Rooms

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,13 +1,18 @@
 (function() {
-  function Room($firebaseArray) {
-    var Room = {};
-    var ref = firebase.database().ref().child("rooms");
-    var rooms = $firebaseArray(ref);
+      function Room($firebaseArray) {
+         var Room = {};
 
-    return {
-      all: rooms
-    };
-  }
+          var ref = firebase.database().ref().child("rooms");
+          var rooms = $firebaseArray(ref);
+
+         Room.all = rooms;
+
+         Room.add = function (room) {
+             rooms.$add(room);
+          };
+
+         return Room;
+      }
 
   angular
     .module('bloc-chat')
